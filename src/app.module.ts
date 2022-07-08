339327bf81be5e0db6ca1fc34111a6d1d3cc509d
shovan777 <raj.shrestha778@gmail.com>
2022-07-08 17:01:09 +0545

    Add environment variable configurations.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
@@ -9,6 +10,10 @@ import { NewsModule } from './news/news.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // isGlobal: true,// Makes the variable global
+      // i.e no need to import in each module
+    }),
     NewsModule,
     // GraphQLModule.forRoot<ApolloDriverConfig>({
     GraphQLModule.forRoot({
@@ -24,11 +29,11 @@ import { NewsModule } from './news/news.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5433,
-      username: 'saleor',
-      password: 'saleor',
-      database: 'nestdb',
-      synchronize: true,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.DB_SYNC === 'true',
       autoLoadEntities: true,
     }),
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { graphqlUploadExpress } from 'graphql-upload';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 5 }));
-  await app.listen(3000);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
