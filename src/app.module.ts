8d381f547d8e3663508dd61e8853b2cd9154cca9
shovan777 <raj.shrestha778@gmail.com>
2022-07-10 12:46:17 +0545

    Add db host to docker env.

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,7 +28,7 @@ import { NewsModule } from './news/news.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
