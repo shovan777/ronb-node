9e02ee973663e84431d2bfffae1383e6b77b03db
shovan777 <raj.shrestha778@gmail.com>
2022-07-12 11:33:14 +0545

    Create news entities' tables in db.

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { NewsService } from './news.service';
 import { NewsResolver } from './news.resolver';
 import { Upload } from 'src/common/scalars/upload.scalar';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { News, NewsImage } from './entities/news.entity';
 
 @Module({
   providers: [NewsResolver, NewsService, Upload],
+  imports: [TypeOrmModule.forFeature([News, NewsImage])],
 })
 export class NewsModule {}
