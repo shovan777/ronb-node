6a36cda350519d0c57455ff69e75fac47ad52ebe
shovan777 <raj.shrestha778@gmail.com>
2022-07-31 12:14:30 +0545

    Add secutiry middleware for auth. Parse the security token from cookie.

---
+++ src/common/middlewares/security.middleware.ts
@@ -0,0 +1,42 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { createClient } from 'redis';
+
+const sub = createClient({
+  url: 'redis://localhost:6379/2',
+});
+const pub = sub.duplicate();
+
+// (async (sub, pub) => {
+//   await pub.connect();
+//   await sub.connect();
+// })(sub, pub);
+// Promise.all([sub, pub])
+//   .then(() => {
+//     console.log('**********redis connected');
+//   })
+//   .catch((err) => {
+//     console.log('not connected');
+//     throw err;
+//   });
+
+@Injectable()
+export class SecurityMiddleware implements NestMiddleware {
+  // use(cookieParser)
+  //   use(cookieParser()
+  use(req: Request, res: Response, next: NextFunction) {
+    // do something
+    console.log('*******hello from security middleware');
+    const cookies = req.cookies;
+    const jwt_auth = cookies.JWT;
+    console.log(cookies);
+    if (!jwt_auth) {
+      console.log('No jwt token');
+      res.status(401).send('Unauthorized');
+      return;
+    }
+
+
+    next();
+  }
+}
