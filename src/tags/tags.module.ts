400f7f0aacd6625695537858d08f43a7386f2a08
shovan777 <raj.shrestha778@gmail.com>
2022-08-10 10:27:21 +0545

    Refactor code spacings.

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -2,23 +2,15 @@ import { forwardRef, Module } from '@nestjs/common';
 import { NewsTaggitService, TagsService } from './tags.service';
 import { NewsTaggitResolver, TagsResolver } from './tags.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import {  NewsTaggit, Tag } from './entities/tag.entity';
+import { NewsTaggit, Tag } from './entities/tag.entity';
 import { NewsModule } from 'src/news/news.module';
 
 @Module({
-  providers: [
-    TagsResolver, 
-    TagsService, 
-    NewsTaggitResolver, 
-    NewsTaggitService,
-  ],
+  providers: [TagsResolver, TagsService, NewsTaggitResolver, NewsTaggitService],
   imports: [
     forwardRef(() => NewsModule),
     TypeOrmModule.forFeature([Tag, NewsTaggit]),
   ],
-  exports: [
-    TagsService,
-    NewsTaggitService,
-  ],
+  exports: [TagsService, NewsTaggitService],
 })
 export class TagsModule {}
