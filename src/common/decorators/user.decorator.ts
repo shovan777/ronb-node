68b6507eede4019883726e12007abbb4517844d7
shovan777 <raj.shrestha778@gmail.com>
2023-01-12 11:02:41 +0545

    Set request user object with role provided by django auth.

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -4,6 +4,6 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = GqlExecutionContext.create(ctx).getContext().req;
-    return request.user;
+    return request.user.id;
   },
 );
