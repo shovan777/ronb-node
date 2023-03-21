2714ba65d36e1a887ef59883e967d09f3def9a7a
shovan777 <raj.shrestha778@gmail.com>
2023-03-21 15:01:14 +0545

    Create news cache microservice.

--- apps/newscache/src/main.ts
+++ apps/newscache/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NewscacheModule } from './newscache.module';
 
 async function bootstrap() {
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   NewscacheModule,
+  //   {
+  //     transport: Transport.TCP,
+  //     options: {
+  //       host: 'localhost',
+  //       port: 4000,
+  //     },
+  //   },
+  // );
   const app = await NestFactory.create(NewscacheModule);
-  await app.listen(4000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 4000,
+    },
+  });
+  await app.startAllMicroservices();
+  await app.listen(3001);
+  // await app.listen();
 }
 bootstrap();
