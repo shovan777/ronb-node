bc217a868c71c8e2464f8451efd1a329524f4498
shovan777 <raj.shrestha778@gmail.com>
2023-04-05 18:24:52 +0545

    Register redis store as cache module for newscache.

--- apps/newscache/src/newscache.module.ts
+++ apps/newscache/src/newscache.module.ts
@@ -1,10 +1,12 @@
 import { SharedModule } from '@app/shared';
 import { News } from '@app/shared/entities/news.entity';
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NewscacheController } from './newscache.controller';
 import { NewscacheService } from './newscache.service';
+import { redisStore } from 'cache-manager-redis-store';
+import { RedisClientOptions } from 'redis';
 
 @Module({
   imports: [
@@ -21,6 +23,15 @@ import { NewscacheService } from './newscache.service';
       database: process.env.DB_NAME,
       synchronize: false,
       autoLoadEntities: true,
+    }),
+    CacheModule.register<RedisClientOptions>({
+      ttl: 60 * 60 * 24 * 7, // 7 days
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      store: async () =>
+        await redisStore({
+          url: process.env.REDIS_URL,
+        }),
     }),
   ],
   controllers: [NewscacheController],
