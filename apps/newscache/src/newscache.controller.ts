5c9a243201d6d593f18a38a034225585c095ebd9
shovan777 <raj.shrestha778@gmail.com>
2023-04-16 15:17:26 +0545

    Begin building news cache for user.

--- apps/newscache/src/newscache.controller.ts
+++ apps/newscache/src/newscache.controller.ts
@@ -1,4 +1,9 @@
+import {
+  BeginCachingResponse,
+  NEWS_CACHING_SERVICE_NAME,
+} from '@app/shared/common/proto/news.pb';
 import { Controller, Get } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
 import { NewscacheService } from './newscache.service';
 
 @Controller()
@@ -6,7 +11,7 @@ export class NewscacheController {
   constructor(private readonly newscacheService: NewscacheService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): Promise<any> {
     return this.newscacheService.getHello();
   }
 
@@ -15,5 +20,12 @@ export class NewscacheController {
     const news_arr = this.newscacheService.findNews();
     console.log(news_arr);
     return this.newscacheService.findNews();
+  }
+
+  @GrpcMethod(NEWS_CACHING_SERVICE_NAME, 'BeginCaching')
+  async beginCaching(data): Promise<BeginCachingResponse> {
+    console.log(`Begin Caching request received ${data}`);
+    this.newscacheService.findNews();
+    return { success: true };
   }
 }
